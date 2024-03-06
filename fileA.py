4c67c4d40b635c2b0eea1ab372dec98750b2c96e
Elyasa <elyasareva@gmail.com>
2024-03-06 14:03:20 +0700

    terserah

---
+++ fileA.py
@@ -0,0 +1,14 @@
+"""
+Contoh skrip pyhton
+"""
+
+print('Praktik GIT pertama')
+
+def kali(a, b):
+    return a * b
+
+if _name_ == '_main_':
+    print(4, 7)
+
+if _umur_ == '_main_':
+    print(4, 8)
